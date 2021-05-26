65b0c699337dcf8c31dde4dac1d9a3d2e53dee30
Johann Rojas <jrojas@modyo.com>
2021-05-26 18:39:55 -0500

    feat: Created Main screen

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import useWeather from '../hooks/useWeather'
+import ActualWeather from './ActualWeather'
+import WeatherForecast from './WeatherForecast'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 const MainScreen = () => {
+  const { currentDay, forecast, isLoading, setLocation } = useWeather(location)
+
   return (
-    <div>
-    </div>
+    <SkeletonTheme color="#100E1D" highlightColor="#444">
+      <div className="row g-0 text-light">
+        <div className="col-12 col-md-4 bg-dark bg-cloud">
+          <ActualWeather isLoading={isLoading} today={currentDay} setLocation={setLocation}/>
+        </div>
+        <div className="col-12 col-md-8 bg-secondary p-0 p-xl-5">
+          <WeatherForecast isLoading={isLoading} today={currentDay} forecast={forecast}/>
+        </div>
+      </div>
+    </SkeletonTheme>
   )
 }
 
